b7d061cd8312120b8b804ef0cc778aaab37b651d
ngvlz <linhvunguyen258@gmail.com>
2021-02-06 15:06:46 -0500

    First update core.py

--- WU255/core.py
+++ WU255/core.py
@@ -1,4 +1,12 @@
 from bs4 import BeautifulSoup
 import sqlite3
 
-# TODO: 
+# TODO: Parse mask price
+
+# TODO: Send email alerts to users
+
+# TODO: Database for daily checker
+
+# TODO: Vaccine status of employees
+
+# TODO: Scrape for general COVID data in the country
